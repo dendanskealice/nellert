eb50ccda1a8b8eea82274a469afb0535108064a5
thatonealise <alice@thatoneali.se>
2022-02-04 01:43:23 +0200

    added logic to request and process wiktionary entries

--- scripts/nellert.js
+++ scripts/nellert.js
@@ -1,12 +1,100 @@
-$(document).ready(function() {   
-    const input = $("#word");
+$(document).ready(function() {
+    const RESPONSE_TYPE_COMMON = 0;
+    const RESPONSE_TYPE_NEUTER = 1;
+    const RESPONSE_TYPE_ERROR = 2;
+    const RESPONSE_TYPE_DEFAULT = 3;
 
+    const responses = [
+        // RESPONSE_TYPE_COMMON
+        {
+            div: $("#response-common"),
+            label: $("#response-common-word"),
+        },
+        // RESPONSE_TYPE_NEUTER
+        {
+            div: $("#response-neuter"),
+            label: $("#response-neuter-word"),
+        },
+        // RESPONSE_TYPE_ERROR
+        {
+            div: $("#response-error"),
+            label: $("#response-error-word"),
+        },
+        // RESPONSE_TYPE_DEFAULT
+        {
+            div: $("#response-default"),
+            label: $("#response-default-word"),
+        },
+    ];
+
+    function respond(responseType, word) {
+        for (const response of responses) {
+            response.div.hide();
+        }
+        let response = responses[responseType];
+        response.label.text(word);
+        response.div.show();
+    }
+
+    respond(RESPONSE_TYPE_DEFAULT, "ord");
+
+    const input = $("#word");
     input.change(function () {
         let word = input.val();
         if (word.trim()) {
-            $.get("https://da.wikipedia.org/w/api.php?action=parse&page=Wikipedia:Unusual_articles/Places_and_infrastructure&prop=wikitext&section=5&format=json", function (data) {
-                console.log(data);
+            $.ajax({
+                url: 'https://en.wiktionary.org/w/api.php',
+                data: {
+                    action: 'parse',
+                    prop: 'text',
+                    page: word,
+                    format: 'json',
+                },
+                dataType:'jsonp',
+                success: function (data) {
+                    if (data.error) {
+                        respond(RESPONSE_TYPE_ERROR, word);
+                        return;
+                    }
+
+                    let entry = $(data.parse.text['*']);
+
+                    let definitions = entry.find("strong.Latn.headword:lang(da):contains(" + word + ")");
+                    if (!definitions.length) {
+                        respond(RESPONSE_TYPE_ERROR, word);
+                        return;
+                    }
+
+                    for (const definition of definitions) {
+                        let category = definition.parentNode;
+                        if (category) {
+                            let gender = $(category).find("span.gender");
+                            console.log(gender);
+                            if (gender.length) {
+                                switch (gender.text()) {
+                                    case "n":
+                                        respond(RESPONSE_TYPE_NEUTER, word);
+                                        return;
+                                    case "c":
+                                        respond(RESPONSE_TYPE_COMMON, word);
+                                        return;
+                                    default:
+                                        console.log("sdf");
+                                        respond(RESPONSE_TYPE_ERROR, word);
+                                        return;
+                                }
+                            }
+                        }
+                    }
+
+                    respond(RESPONSE_TYPE_ERROR, word);
+                },
+                failure: function () {
+                    respond(RESPONSE_TYPE_ERROR, word);
+                }
             });
+        } else {
+            respond(RESPONSE_TYPE_DEFAULT, "ord");
         }
     });
 });
